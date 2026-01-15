5417bd76d6a5bda42bc28b132fe214ab0c461481
chi.dev <makhortykh.ruslan@gmail.com>
2026-01-15 23:04:06 +0200

    DELETE: Signed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react';
 import AppRouters from './AppRouters.tsx';
 import { AppQuizletProvider } from './core/contexts/AppContext.tsx';
-import Signed from './core/components/Signed/Signed.tsx';
 
 const App: FC = () => (
   <div>
     <AppQuizletProvider>
       <AppRouters />
     </AppQuizletProvider>
-    <Signed />
   </div>
 );
 
